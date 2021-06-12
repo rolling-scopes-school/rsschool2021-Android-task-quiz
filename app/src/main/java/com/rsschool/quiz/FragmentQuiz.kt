2e1f270adc9e7131471e04c8d4147fcce5b3204c
Bondidos <gglbond@gmail.com>
2021-06-13 01:34:31 +0300

    commit: force to work binding ;)

---
+++ app/src/main/java/com/rsschool/quiz/FragmentQuiz.kt
@@ -0,0 +1,29 @@
+package com.rsschool.quiz
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.rsschool.quiz.databinding.FragmentQuizBinding
+
+class FragmentQuiz : Fragment() {
+
+    private var _binding: FragmentQuizBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentQuizBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.rsschool.quiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.rsschool.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        openQuiz()
+    }
+    private fun openQuiz (){
+        val fragment : Fragment = FragmentQuiz()
+        val transaction :FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.add(binding.Container.id,fragment)
+        transaction.commit()
     }
 }
